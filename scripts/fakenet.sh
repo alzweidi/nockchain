e2641911329a54a522223bf89eed50adfa69a0ca
alzweidi <133758240+alzweidi@users.noreply.github.com>
2025-06-09 12:42:52 +0100

    Update fakenet.sh

--- scripts/fakenet.sh
+++ scripts/fakenet.sh
@@ -1,2 +1,2 @@
 export RUST_LOG=info
-../target/release/nockchain --fakenet --genesis-leader --npc-socket nockchain.sock --mining-pubkey "3oSuHZHPFtioRAxzGDgC4xL6XZBETXKU5uLkEJfZk12kkiph5JckPT2HQYaqwA8PigkhvwKv5nDEs8wLaX51o5Pr9x3aL746HH5YaDsV3S5nubiEMbVsHJFByKXGcFAATzVb" --bind /ip4/0.0.0.0/udp/3005/quic-v1 --peer /ip4/127.0.0.1/udp/3006/quic-v1 --new-peer-id --no-default-peers --mine
+../target/release/nockchain --fakenet --genesis-leader --npc-socket nockchain.sock --mining-pubkey "2sZFDAmx3fuDq4JNgqHiwj3Gp6ysaZ8pYxGyXjeWWSii4yX54B6wpNBiMLAatkKnAMcw6P1Y8LzcgEqZYX4CRDK81ceCzngZD9gu3qSixyxjyJUMFUgNDAv5imJXkbMEYMSy" --bind /ip4/0.0.0.0/udp/3005/quic-v1 --peer /ip4/127.0.0.1/udp/3006/quic-v1 --new-peer-id --no-default-peers --mine
